1fb8a6a20b664163af92a2f91bd672e0c0dc86e5
Andrew Wilkins <axwalk@gmail.com>
2017-04-24 19:09:07 +0800

    Add cloud-init templates

---
+++ templates.go
@@ -0,0 +1,50 @@
+package lxdimage
+
+const (
+	cloudInitMetaTemplate = `#cloud-config
+instance-id: {{ container.name }}
+local-hostname: {{ container.name }}
+{{ config_get("user.meta-data", "") }}`
+
+	cloudInitNetworkTemplate = `{% if config_get("user.network-config", "") == "" %}version: 1
+config:
+    - type: physical
+      name: eth0
+      subnets:
+          - type: {% if config_get("user.network_mode", "") == "link-local" %}manual{% else %}dhcp{% endif %}
+            control: auto{% else %}{{ config_get("user.network-config", "") }}{% endif %}`
+
+	cloudInitUserTemplate = `{{ config_get("user.user-data", properties.default) }}`
+
+	cloudInitVendorTemplate = `{{ config_get("user.vendor-data", properties.default) }}`
+)
+
+// CloudInitTemplates contains a set of templates that can be used
+// to create cloud-init metadata files.
+var CloudInitTemplates = []Template{{
+	Template: "cloud-init-meta.tpl",
+	When:     []string{"create", "copy"},
+	Path:     "/var/lib/cloud/seed/nocloud-net/meta-data",
+	Content:  cloudInitMetaTemplate,
+}, {
+	Template: "cloud-init-network.tpl",
+	When:     []string{"create", "copy"},
+	Path:     "/var/lib/cloud/seed/nocloud-net/network-config",
+	Content:  cloudInitNetworkTemplate,
+}, {
+	Properties: map[string]string{
+		"default": "#cloud-config\n{}",
+	},
+	Template: "cloud-init-user.tpl",
+	When:     []string{"create", "copy"},
+	Path:     "/var/lib/cloud/seed/nocloud-net/user-data",
+	Content:  cloudInitUserTemplate,
+}, {
+	Properties: map[string]string{
+		"default": "#cloud-config\n{}",
+	},
+	Template: "cloud-init-vendor.tpl",
+	When:     []string{"create", "copy"},
+	Path:     "/var/lib/cloud/seed/nocloud-net/vendor-data",
+	Content:  cloudInitVendorTemplate,
+}}
